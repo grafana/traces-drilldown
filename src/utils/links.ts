fe6b9a5845428073034e8fe65f2396598cc0c307
Piotr Jamróz <pm.jamroz@gmail.com>
2025-04-09 12:24:38 +0200

    Explore: Add "Go Queryless" hook

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,12 +1,12 @@
 import {
   PluginExtensionAddedLinkConfig,
   PluginExtensionPanelContext,
-  PluginExtensionPoints,
+  PluginExtensionPoints, RawTimeRange,
   toURLRange
 } from '@grafana/data';
 
-import { DataSourceRef } from '@grafana/schema';
-import { EXPLORATIONS_ROUTE, VAR_DATASOURCE, VAR_FILTERS, VAR_METRIC } from './shared';
+import {DataQuery, DataSourceRef} from '@grafana/schema';
+import {EXPLORATIONS_ROUTE, VAR_DATASOURCE, VAR_FILTERS, VAR_METRIC} from './shared';
 
 type TempoQuery = {
   filters?: TraceqlFilter[];
@@ -20,20 +20,37 @@ export interface TraceqlFilter {
   value?: (string | string[]);
 }
 
-const title = 'Open in Explore Traces';
-const description = 'Open current query in the Explore Traces app';
+type PluginExtensionExploreContext = {
+  targets: DataQuery[];
+  timeRange: RawTimeRange;
+};
 
 export const linkConfigs: Array<{ targets: string | string[] } & PluginExtensionAddedLinkConfig<PluginExtensionPanelContext>> = [
   {
     targets: PluginExtensionPoints.DashboardPanelMenu,
-    title,
-    description,
+    title: 'Open in Explore Traces',
+    description: 'Open current query in the Explore Traces app',
     path: createAppUrl(),
-    configure: contextToLink,
+    configure: (context?: PluginExtensionPanelContext) => contextToLink(context),
   },
+  {
+    targets: PluginExtensionPoints.ExploreToolbarAction,
+    title: "Open in Grafana Traces Drilldown",
+    description: 'Try our new queryless experience for traces',
+    path: createAppUrl(),
+    configure: (context?: PluginExtensionExploreContext) => contextToLink(context),
+  }
 ];
 
-export function contextToLink<T extends PluginExtensionPanelContext>(context?: T) {
+/**
+ * Props shared between ...
+ */
+type ContextToLinkProps = {
+  targets: DataQuery[],
+  timeRange: RawTimeRange,
+}
+
+export function contextToLink(context?: ContextToLinkProps) {
   if (!context) {
     return undefined;
   }
@@ -64,11 +81,11 @@ export function contextToLink<T extends PluginExtensionPanelContext>(context?: T
 
   const getFilters = (filters: TraceqlFilter[]) => {
     return filters
-      .filter((filter) => filter.tag !== 'status')
-      .map((filter) => `${filter.scope}.${filter.tag}|${filter.operator}|${filter.value}`);
+        .filter((filter) => filter.tag !== 'status')
+        .map((filter) => `${filter.scope}.${filter.tag}|${filter.operator}|${filter.value}`);
   };
   getFilters(filters).forEach((filter) => params.append(`var-${VAR_FILTERS}`, filter));
-    
+
   const url = createAppUrl(params);
   return {
     path: `${url}`,
