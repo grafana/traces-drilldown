bee79cde594d912fb4243646de395dd630a58116
Sven Grossmann <svennergr@gmail.com>
2025-04-10 17:21:38 +0200

    chore: remove extension link from logs drilldown

--- src/module.tsx
+++ src/module.tsx
@@ -1,9 +1,6 @@
 import { lazy } from 'react';
 import { AppPlugin } from '@grafana/data';
 
-// @ts-ignore new API that is not yet in stable release
-import { sidecarServiceSingleton_EXPERIMENTAL } from '@grafana/runtime';
-import pluginJson from './plugin.json';
 import { exposedComponents } from 'exposedComponents';
 import { linkConfigs } from 'utils/links';
 
@@ -15,15 +12,6 @@ export const plugin = new AppPlugin<{}>().setRootPage(App).addConfigPage({
   icon: 'cog',
   body: AppConfig,
   id: 'configuration',
-})
-.addLink({
-  title: 'traces drilldown',
-  description: 'Open in Traces Drilldown',
-  icon: 'align-left',
-  targets: 'grafana-lokiexplore-app/toolbar-open-related/v1',
-  onClick: () => {
-    sidecarServiceSingleton_EXPERIMENTAL?.openAppV3({ pluginId: pluginJson.id, path: '/explore' });
-  },
 });
 
 for (const linkConfig of linkConfigs) {
