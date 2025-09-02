0a29c61d8caee393a251ec1f804e4a965e506669
Joey <joey.tawadrous@grafana.com>
2025-09-02 09:42:21 +0100

    Simplify rendering and time range

--- src/addedComponents/InsightsTimelineWidget/InsightsTimelineWidget.tsx
+++ src/addedComponents/InsightsTimelineWidget/InsightsTimelineWidget.tsx
@@ -1,6 +1,6 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+import React, { ReactElement } from 'react';
 
-import { GrafanaTheme2, TimeRange } from '@grafana/data';
+import { GrafanaTheme2 } from '@grafana/data';
 import { usePluginComponent } from '@grafana/runtime';
 import { sceneGraph, SceneObject } from '@grafana/scenes';
 import { css } from '@emotion/css';
@@ -39,12 +39,12 @@ export interface Cluster {
 
 interface InsightsTimelineWidgetProps {
   serviceName: string;
-  start: string;
-  end: string;
+  start: string | number;
+  end: string | number;
   filterBySeverity?: AssertionSeverity[];
   filterBySummaryKeywords?: string[];
   isEmbeddedApp?: boolean;
-  dataCallback: (data: Record<string, { healthStates: HealthState[], clusters: Cluster[] }>) => void;
+  label?: ReactElement;
 }
 
 export type InsightsTimelineWidgetExternal = (props: InsightsTimelineWidgetProps) => ReactElement | null;
@@ -58,28 +58,10 @@ export function InsightsTimelineWidget({ serviceName, model }: Props) {
   const { isLoading, component: InsightsTimelineWidgetExternal } = usePluginComponent<InsightsTimelineWidgetProps>(
     'grafana-asserts-app/insights-timeline-widget/v1'
   );
-  const [timeRange, setTimeRange] = useState<TimeRange>();
-  const [showTitle, setShowTitle] = useState(false);
-  const [showInsights, setShowInsights] = useState(true);
   const styles = useStyles2(getStyles);
   const traceExploration = getTraceExplorationScene(model);
   const { embedded } = traceExploration.useState();
   const sceneTimeRange = sceneGraph.getTimeRange(model).useState();
-
-  useEffect(() => {
-    setTimeRange(sceneTimeRange.value);
-    setShowInsights(true);  // reset to true to show insights, which will call checkHasData()
-  }, [sceneTimeRange]);
-
-  const checkHasData = (data: Record<string, { healthStates: HealthState[], clusters: Cluster[] }>) => {
-    const clustersAndHealthStates = Array.isArray(data) ? data : Object.values(data);
-    const hasData = clustersAndHealthStates.some((clustersAndHealthState) => {
-      const healthCount = clustersAndHealthState?.healthStates?.length ?? 0;
-      return healthCount > 0;
-    });
-    setShowTitle(hasData);
-    setShowInsights(hasData);
-  };
 
   const metric = getMetricVariable(model).state.value as MetricFunction;
   let filterBySeverity: AssertionSeverity[] = [];
@@ -94,53 +76,36 @@ export function InsightsTimelineWidget({ serviceName, model }: Props) {
     filterBySummaryKeywords = ['latency'];
   }
   
-  if (isLoading || !InsightsTimelineWidgetExternal || !timeRange) {
+  if (isLoading || !InsightsTimelineWidgetExternal || !sceneTimeRange) {
+    return null;
+  }
+
+  const element = <InsightsTimelineWidgetExternal
+    serviceName={serviceName}
+    start={sceneTimeRange.from.valueOf()}
+    end={sceneTimeRange.to.valueOf()}
+    filterBySeverity={filterBySeverity}
+    filterBySummaryKeywords={filterBySummaryKeywords}
+    label={<div className={styles.label}>Insights</div>}
+    isEmbeddedApp={embedded}
+  />
+
+  if (element === null) {
     return null;
   }
 
   return (
-    <div className={styles.insightsContainer}>
-      {showTitle && (
-        <div className={styles.insightsTitle}>
-          Insights
-        </div>
-      )}
-      {showInsights && (
-        <div className={styles.insightsTimelineContainer}>
-          <InsightsTimelineWidgetExternal
-            serviceName={serviceName}
-            start={timeRange.from.toISOString()}
-            end={timeRange.to.toISOString()}
-            filterBySeverity={filterBySeverity}
-            filterBySummaryKeywords={filterBySummaryKeywords}
-            dataCallback={(data: Record<string, { healthStates: HealthState[], clusters: Cluster[] }>) => checkHasData(data)}
-            isEmbeddedApp={embedded}
-          />
-        </div>
-      )}
-    </div>
+    <>{element}</>
   );
 }
 
 function getStyles(theme: GrafanaTheme2) {
   return {
-    insightsContainer: css({
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'center',
-      gap: '8px',
-      marginBottom: '6px',
-    }),
-    insightsTitle: css({
+    label: css({
       fontSize: '12px',
       color: theme.colors.text.secondary,
       marginLeft: '35px',
       marginTop: '-3px',
-    }),
-    insightsTimelineContainer: css({
-      marginLeft: '10px',
-      marginRight: '15px',
-      width: '100%',
     }),
   };
 }
