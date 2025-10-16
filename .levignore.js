e4b33362ec8ac2a8ffbb6fe31983c125e61dcb58
Piotr Jamróz <pm.jamroz@gmail.com>
2025-10-16 13:02:24 +0200

    update levignore

--- .levignore.js
+++ .levignore.js
@@ -1,7 +1,7 @@
 module.exports = {
   removals: [
-    /getPluginLinkExtensions/, // removed in 12.0.0
-    /DataLinksCustomContext/, // added in 12.3.0
-    /useDataLinksContext/, // added in 12.3.0
+    /getPluginLinkExtensions/, // removed in 12.0.0 (https://github.com/grafana/grafana/pull/102102)
+    /DataLinksContext/, // added in 12.3.0 (https://github.com/grafana/grafana/pull/110590)
+    /useDataLinksContext/, // added in 12.3.0 (https://github.com/grafana/grafana/pull/110590)
   ],
 };
--- src/components/Explore/TracesByService/REDPanel.tsx
+++ src/components/Explore/TracesByService/REDPanel.tsx
@@ -37,8 +37,6 @@ import { buildHistogramQuery } from '../queries/histogram';
 import { isEqual } from 'lodash';
 import { DurationComparisonControl } from './DurationComparisonControl';
 import { exemplarsTransformations, removeExemplarsTransformation } from '../../../utils/exemplars';
-import { InsightsTimelineWidget } from 'addedComponents/InsightsTimelineWidget/InsightsTimelineWidget';
-import { useServiceName } from 'pages/Explore/TraceExploration';
 
 export interface RateMetricsPanelState extends SceneObjectState {
   panel?: SceneFlexLayout;
@@ -262,7 +260,6 @@ export class REDPanel extends SceneObjectBase<RateMetricsPanelState> {
     const { panel, actions, isStreaming } = model.useState();
     const { value: metric } = getMetricVariable(model).useState();
     const styles = useStyles2(getStyles);
-    const serviceName = useServiceName(model);
 
     if (!panel) {
       return;
@@ -312,10 +309,6 @@ export class REDPanel extends SceneObjectBase<RateMetricsPanelState> {
           </div>
         </div>
         <panel.Component model={panel} />
-        <InsightsTimelineWidget
-          serviceName={serviceName || ''}
-          model={model}
-        />
       </div>
     );
   };
