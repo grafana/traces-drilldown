6ea96dd33b3ae319c555d2458e4b712b0bead0e8
Piotr Jamróz <pm.jamroz@gmail.com>
2025-10-24 12:36:11 +0200

    feat: Link to Logs Drilldown instead of Explore (#563)
    * feat: Link to Logs Drilldown instead of Explore
    
    * update levignore
    
    * update levignore
    
    * update condition
    
    * revert local changes

--- .levignore.js
+++ .levignore.js
@@ -1,3 +1,7 @@
 module.exports = {
-  removals: [/getPluginLinkExtensions/, /PluginExtensionLinkConfig/],
+  removals: [
+    /getPluginLinkExtensions/, // removed in 12.0.0 (https://github.com/grafana/grafana/pull/102102)
+    /DataLinksContext/, // added in 12.3.0 (https://github.com/grafana/grafana/pull/110590)
+    /useDataLinksContext/, // added in 12.3.0 (https://github.com/grafana/grafana/pull/110590)
+  ],
 };
