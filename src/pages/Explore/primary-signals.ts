a543fa4fdb0c1502a14122f905522d25c8847f6e
Hugo Häggmark <hugo.haggmark@gmail.com>
2026-08-27 14:40:14 +0200

    test: add primary signal option test ids

--- src/pages/Explore/primary-signals.ts
+++ src/pages/Explore/primary-signals.ts
@@ -1,4 +1,5 @@
 import { SelectableValue } from '@grafana/data';
+import { testIds } from 'utils/testIds';
 
 export const DATABASE_CALLS_KEY = 'span.db.system.name';
 
@@ -8,30 +9,35 @@ export const primarySignalOptions: Array<SelectableValue<string>> = [
     value: 'nestedSetParent<0',
     filter: { key: 'nestedSetParent', operator: '<', value: '0' },
     description: 'Focus your analysis on the root span of each trace',
+    dataTestId: testIds.primarySignalOptionRootSpans,
   },
   {
     label: 'All spans',
     value: 'true',
     filter: { key: '', operator: '', value: true },
     description: 'View and analyse raw span data. This option may result in long query times.',
+    dataTestId: testIds.primarySignalOptionAllSpans,
   },
   {
     label: 'Server spans',
     value: 'kind=server',
     filter: { key: 'kind', operator: '=', value: 'server' },
     description: 'Explore server-specific segments of traces',
+    dataTestId: testIds.primarySignalOptionServerSpans,
   },
   {
     label: 'Consumer spans',
     value: 'kind=consumer',
     filter: { key: 'kind', operator: '=', value: 'consumer' },
     description: 'Analyze interactions initiated by consumer services',
+    dataTestId: testIds.primarySignalOptionConsumerSpans,
   },
   {
     label: 'Database calls',
     value: `${DATABASE_CALLS_KEY}!=""`,
     filter: { key: DATABASE_CALLS_KEY, operator: '!=', value: '""' },
     description: 'Evaluate the performance issues in database interactions',
+    dataTestId: testIds.primarySignalOptionDatbaseCalls,
   },
 ];
 
