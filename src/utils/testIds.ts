5983f7aeddfc850677e3e1ee50beb5370d550d6f
Hugo Häggmark <hugo.haggmark@gmail.com>
2026-09-08 06:16:24 +0200

    test: add primary signal option test ids (#883)

--- src/utils/testIds.ts
+++ src/utils/testIds.ts
@@ -1,17 +1,24 @@
 import { VariableValue } from '@grafana/scenes';
 import { AttributeItem } from '../types';
+import pluginJson from '../plugin.json';
 
 export const testIds = {
-  emptyState: 'data-testid empty-state',
-  errorState: 'data-testid error-state',
-  loadingState: 'data-testid loading-state',
-  ratePanel: `data-testid rate-panel`,
-  errorsPanel: `data-testid errors-panel`,
-  durationPanel: `data-testid duration-panel`,
-  breakdownContainer: `data-testid breakdown container`,
-  serviceStructureContainer: `data-testid service structure container`,
-  comparisonContainer: `data-testid comparison container`,
-  tracesContainer: `data-testid traces container`,
+  emptyState: `${pluginJson.id} empty-state`,
+  errorState: `${pluginJson.id} error-state`,
+  loadingState: `${pluginJson.id} loading-state`,
+  ratePanel: `${pluginJson.id} rate-panel`,
+  errorsPanel: `${pluginJson.id} errors-panel`,
+  durationPanel: `${pluginJson.id} duration-panel`,
+  breakdownContainer: `${pluginJson.id} breakdown container`,
+  serviceStructureContainer: `${pluginJson.id} service structure container`,
+  comparisonContainer: `${pluginJson.id} comparison container`,
+  tracesContainer: `${pluginJson.id} traces container`,
+  primarySignalOptionRootSpans: `${pluginJson.id} primary signal option root spans`,
+  primarySignalOptionAllSpans: `${pluginJson.id} primary signal option all spans`,
+  primarySignalOptionServerSpans: `${pluginJson.id} primary signal option server spans`,
+  primarySignalOptionConsumerSpans: `${pluginJson.id} primary signal option consumer spans`,
+  primarySignalOptionDatbaseCalls: `${pluginJson.id} primary signal option database calls`,
+  primarySignalSelect: `${pluginJson.id} primary signal select`,
 };
 
 export function getTestIdFromMetric(metric: VariableValue | string): string {
@@ -25,12 +32,12 @@ export function getTestIdFromMetric(metric: VariableValue | string): string {
       return testIds.durationPanel;
   }
 
-  return 'data-testid unknown-panel';
+  return 'unknown-panel';
 }
 
 export function getTestIdFromAttribute(attribute: AttributeItem): string {
   if (!attribute?.value) {
-    return 'data-testid unknown-attribute-item';
+    return `${pluginJson.id} unknown-attribute-item`;
   }
-  return `data-testid ${attribute.value}-attribute-item`;
+  return `${pluginJson.id} ${attribute.value}-attribute-item`;
 }
